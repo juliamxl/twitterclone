27b6efcb24a4abf2de68757ecab950441d2df3a0
Julia Marçal <juliaamarxal@gmail.com>
2023-04-05 20:48:30 -0300

    ajustes commit

---
+++ components/layout/SidebarItem.tsx
@@ -0,0 +1,17 @@
+import { IconType } from "react-icons/lib";
+
+interface SidebarItemProps{
+    label: string;
+    href?: string;
+    icon: IconType
+}
+
+const SidebarItem = () => {
+    return(
+        <div>
+
+        </div>
+    );
+}
+
+export default SidebarItem
