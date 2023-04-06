bced7b772079cba959bc58dfe4a2b34d9d0b0bdb
Julia Marçal <juliaamarxal@gmail.com>
2023-04-06 14:44:24 -0300

    Modal.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -6,7 +6,7 @@ import Modal from '@/components/Modal'
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
-    <Modal/>
+    <Modal isOpen tittle='test modal'/>
       <Layout>
         <Component {...pageProps} />
       </Layout>
